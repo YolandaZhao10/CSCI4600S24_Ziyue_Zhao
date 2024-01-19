978e1ef74918fd7fcbc683eec296fada6f67e62a
YolandaZhao10 <zzyuer001@163.com>
2024-01-19 16:21:24 -0500

    lab 0 and data list

---
+++ lab0/lab0.R
@@ -0,0 +1,39 @@
+library(ISLR)
+data(Auto)
+head(Auto)
+names(Auto)
+summary(Auto)
+summary(Auto$mpg)
+fivenum(Auto$mpg)
+boxplot(Auto$mpg)
+hist(Auto$mpg)
+summary(Auto$horsepower)
+summary(Auto$weight)
+fivenum(Auto$weight)
+boxplot(Auto$weight)
+mean(Auto$weight)
+median((Auto$weight))
+
+#read csv
+EPI_data <- read.csv("c:/code/CSCI4600S24_Ziyue_Zhao/Lab1/2010EPI_data.csv")
+EPI_data_name <- EPI_data[1,]
+EPI_data = EPI_data[-1,]
+colnames(EPI_data) <- c(EPI_data_name)
+
+#read xls
+library(readxl)
+excel_sheets('c:/code/CSCI4600S24_Ziyue_Zhao/Lab1/2010EPI_data.xls')
+EPI_data1 <- read_xls('c:/code/CSCI4600S24_Ziyue_Zhao/Lab1/2010EPI_data.xls',sheet = 4)
+
+#choose file
+help(read.csv)
+data1 <- read.csv(file.choose(),header=T)
+
+
+fivenum(EPI_data$EPI)
+summary(EPI_data$EPI)
+plot(EPI_data$EPI,EPI_data$Population07)
+boxplot(EPI_data$EPI)
+hist(EPI_data$EPI)
+
+pt(q=-1.549, df=14, lower.tail = TRUE)
