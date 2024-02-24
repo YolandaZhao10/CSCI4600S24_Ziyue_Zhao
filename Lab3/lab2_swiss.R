f72c42a66eeff41f03767ec2e04aabbb29fce227
YolandaZhao10 <zzyuer001@163.com>
2024-02-23 22:56:41 -0500

    finish lab3

--- Lab3/lab2_swiss.R
+++ Lab3/lab2_swiss.R
@@ -4,7 +4,5 @@ sclass <- kmeans(swiss[1:6], 3)
 table(sclass$cluster, swiss[,2])    
 # 
 library(e1071)
-nam <- swiss[, -2]
-m <- naiveBayes(nam, swiss[, 2])
-table(predict(m, nam, swiss[, 2]))
+m <- naiveBayes(swiss[1:6], swiss[,2]) 
 
